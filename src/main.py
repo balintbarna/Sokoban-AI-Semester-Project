64ebebd0ff00daa29752226982945aabccc1bf42
Balint Kovari <balintmaci@gmail.com>
2019-09-27 15:40:27 +0200

    added basics to main

--- src/main.py
+++ src/main.py
@@ -3,10 +3,19 @@
 import ev3dev.ev3 as ev3
 from time import sleep
 
+import driver.color_sensor as clr
+import driver.motor as mtr
+import driver.touch as touch
+
 import signal
 
-mA = ev3.LargeMotor('outA')
-mB = ev3.LargeMotor('outB')
+def close():
+	ev3.Sound.beep().wait()
+	mtr.stop()
+	exit(0)
+
+btn = ev3.Button()
+
 
 THRESHOLD_LEFT = 30 
 THRESHOLD_RIGHT = 350
@@ -14,43 +23,34 @@ THRESHOLD_RIGHT = 350
 BASE_SPEED = 30
 TURN_SPEED = 80
 
-#lightSensorLeft = ev3.ColorSensor('in1')
-#lightSensorRight = ev3.LightSensor('in2') 
-
-#assert lightSensorLeft.connected, "LightSensorLeft(ColorSensor) is not connected"
-#assert lightSensorRight.connected, "LightSensorRight(LightSensor) is not conected"
-
-mB.run_direct()
-mA.run_direct()
-
-
-mA.polarity = "inversed"
-mB.polarity = "inversed"
 
 def signal_handler(sig, frame):
 	print('Shutting down gracefully')
-	mA.duty_cycle_sp = 0
-	mB.duty_cycle_sp = 0
-
-	exit(0)
+	close()
 
 signal.signal(signal.SIGINT, signal_handler)
 print('Press Ctrl+C to exit')
 
-
+mtr.setDuty(BASE_SPEED)
 while True:
-#	sensorLeft = lightSensorLeft.value()
-#	sensorRight = lightSensorRight.value()
-
-#	print("sensorLeft: ", sensorLeft, " sensorRight: ", sensorRight)
-#	if sensorRight < THRESHOLD_RIGHT:
-#		mA.duty_cycle_sp = TURN_SPEED
-#	else:
-#		mA.duty_cycle_sp = BASE_SPEED
 	
+	tou_val = touch.get()
 
-#	if sensorLeft < THRESHOLD_LEFT:
-	mB.duty_cycle_sp = BASE_SPEED
-#	else:
-	mA.duty_cycle_sp = BASE_SPEED
+	if tou_val > 0:
+		close()
+
+	if btn.any():
+		close()
+
+
+
+	leftLight = clr.getLeft()
+	rightLight = clr.getRight()
+	diff = leftLight - rightLight
+	# positive diff means turn left
+	modifier = 0.2
+	leftSpeed = BASE_SPEED - diff * modifier
+	rightSpeed = BASE_SPEED + diff * modifier
+	mtr.setDutyLR(leftSpeed, rightSpeed)
+	print(diff)
 
