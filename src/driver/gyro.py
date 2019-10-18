1487455106407259c1705079eec1cd952263cbfd
Balint Kovari <balintmaci@gmail.com>
2019-10-18 21:16:48 +0200

    fix basic gyro driver functions

--- src/driver/gyro.py
+++ src/driver/gyro.py
@@ -4,17 +4,21 @@ import driver.ports as prt
 sensor = ev3.GyroSensor(prt.gyroPort)
 assert sensor.connected, "Gyro is not connected"
 
-sensor.mode = 'GYRO-ANG'
+sensor.mode = sensor.MODE_GYRO_ANG
+dec = sensor.decimals
 
 print('Gyro units: ' + sensor.units)
 
 offset = 0.0
 
 def raw():
-    return sensor.value()
+    return sensor.value() / dec
 
 def get():
     return sensor.value() - offset
 
 def reset():
     offset = raw()
+
+def rate():
+    return sensor.rate
