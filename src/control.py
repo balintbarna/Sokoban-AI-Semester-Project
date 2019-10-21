116fd5154586862361bd7877850c8ff986b89c2a
Balint Kovari <balintmaci@gmail.com>
2019-10-21 15:21:55 +0200

    comments on control functions

--- src/control.py
+++ src/control.py
@@ -10,6 +10,9 @@ from simple_pid import PID
 BASE_SPEED = 60
 line_pid = PID(0.2,0.0,0.02,0.0) # PID object for line follower
 
+# this function uses the difference between the two color sensor values as the input for a PID controller
+# the output will modify the base speed of the motors to make the robot turn
+# this control should make the robot go straight on the line
 def line_control():
     global line_pid
     leftLight = clr.getLeft()
@@ -30,20 +33,27 @@ def line_control():
 
 turn_pid = PID(2.0, 0.0, 0.2, 0.0)
 
+# this function sets up the control for the turning
+# it sets the setpoint for the controller (for example 90 degress)
+# it resets the controller and zeroes the gyro sensor to prepare
 def turn_setup(set_deg = 0.0):
     global turn_pid
     turn_pid.setpoint = set_deg
     turn_pid.reset()
     gyro.reset()
 
+# this function is to be called repeatedly until the turn is finished
+# this will use the controller previously set up by calling turn_setup()
+# this will turn the robot with an appropriate rate until it is repeatedly called
 def turn_control():
     global turn_pid
     val = turn_pid(gyro.get())
     mtr.setDutyLR(val, 0 - val)
 
-
 # CHECKER
 
+# tells if the sensors are right above an intersection
+# constants need to be configured right to detect blackness
 def is_intersection():
     leftLight = clr.getLeft()
     rightLight = clr.getRight()
@@ -53,6 +63,8 @@ def is_intersection():
     else:
         return False
 
+# tells if the turn, which was setup with turn_setup(), is completed, by reading the gyro values
+# completion treshhol needs to be configured properly
 def is_turn_finished():
     global turn_pid
     setp = turn_pid.setpoint
