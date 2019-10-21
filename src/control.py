1bc1387d0a1e6a614d1774fd47d8b18a8500ce7d
Balint Kovari <balintmaci@gmail.com>
2019-10-21 15:30:12 +0200

    make nice function docs for control

--- src/control.py
+++ src/control.py
@@ -10,10 +10,13 @@ from simple_pid import PID
 BASE_SPEED = 60
 line_pid = PID(0.2,0.0,0.02,0.0) # PID object for line follower
 
-# this function uses the difference between the two color sensor values as the input for a PID controller
-# the output will modify the base speed of the motors to make the robot turn
-# this control should make the robot go straight on the line
+
 def line_control():
+    """
+    This function uses the difference between the two color sensor values as the input for a PID controller.
+    The output will modify the base speed of the motors to make the robot turn.
+    This control, called repeatedly, should make the robot follow the line.
+    """
     global line_pid
     leftLight = clr.getLeft()
     rightLight = clr.getRight()
@@ -33,28 +36,37 @@ def line_control():
 
 turn_pid = PID(2.0, 0.0, 0.2, 0.0)
 
-# this function sets up the control for the turning
-# it sets the setpoint for the controller (for example 90 degress)
-# it resets the controller and zeroes the gyro sensor to prepare
 def turn_setup(set_deg = 0.0):
+    """
+    This function sets up the control for the turning.
+    It sets the setpoint for the controller (for example 90 degress).
+    It resets the controller and zeroes the gyro sensor to prepare.
+    This should be called right before you start turning at an intersection.
+    """
     global turn_pid
     turn_pid.setpoint = set_deg
     turn_pid.reset()
     gyro.reset()
 
-# this function is to be called repeatedly until the turn is finished
-# this will use the controller previously set up by calling turn_setup()
-# this will turn the robot with an appropriate rate until it is repeatedly called
+
 def turn_control():
+    """
+    This function is to be called repeatedly until the turn is finished.
+    This will use the controller previously set up by calling turn_setup().
+    This will turn the robot with an appropriate rate until it is repeatedly called.
+    """
     global turn_pid
     val = turn_pid(gyro.get())
     mtr.setDutyLR(val, 0 - val)
 
 # CHECKER
 
-# tells if the sensors are right above an intersection
-# constants need to be configured right to detect blackness
+
 def is_intersection():
+    """
+    Tells if the sensors are right above an intersection.
+    Constants need to be configured right to detect blackness.
+    """
     leftLight = clr.getLeft()
     rightLight = clr.getRight()
 
@@ -63,9 +75,11 @@ def is_intersection():
     else:
         return False
 
-# tells if the turn, which was setup with turn_setup(), is completed, by reading the gyro values
-# completion treshhol needs to be configured properly
 def is_turn_finished():
+    """
+    Tells if the turn, which was setup with turn_setup(), is completed, by reading the gyro values.
+    Completion treshhol needs to be configured properly.
+    """
     global turn_pid
     setp = turn_pid.setpoint
     actual = gyro.get()
