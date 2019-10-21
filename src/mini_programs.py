9240f3ec86fc4994e0a8a2f7ce0ab0cb42d5b907
Balint Kovari <balintmaci@gmail.com>
2019-10-21 15:55:52 +0200

    added docs on mini program functions also changed line ending

--- src/mini_programs.py
+++ src/mini_programs.py
@@ -5,12 +5,19 @@ import driver.gyro as gyro
 import control as ctrl
 
 
-
 def go_until_intersection():
+    """
+    This function will make the robot follow the line until it reaches an intersection.
+    The function is blocking and will return when the intersection is reached.
+    """
     while(ctrl.is_intersection() == False):
         ctrl.line_control()
 
-def turn(set_deg = 0):
+def turn(set_deg):
+    """
+    This function will make the robot turn the degrees given as a parameter.
+    It will set up the controller, execute the turn and block until the set result is reached (with a set accuracy)
+    """
     ctrl.turn_setup(set_deg)
     while(ctrl.is_turn_finished() == False):
         ctrl.turn_control()
