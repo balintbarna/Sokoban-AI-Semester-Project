e7ef100371105d2d8ee4b37d568d515c4f2587ac
Balint Kovari <balintmaci@gmail.com>
2019-10-21 15:11:41 +0200

    commenting color sensor driver

--- src/driver/color_sensor.py
+++ src/driver/color_sensor.py
@@ -1,12 +1,14 @@
 import ev3dev.ev3 as ev3
 import driver.ports as prt
 
+# convenience functions to get sensor values
 def getLeft():
     return left.value()
 
 def getRight():
     return right.value()
 
+# load, check connection, set mode
 left = ev3.ColorSensor(prt.leftColorSensorPort)
 right = ev3.ColorSensor(prt.rightColorSensorPort)
 assert left.connected, "Left Color sensor is not connected"
